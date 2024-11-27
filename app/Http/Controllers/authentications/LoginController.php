5c276db744dbe6d9b455b505fb89f13287af43bb
Jesús Ramírez <jarp8>
2024-11-27 17:24:02 -0600

    Intented route on login controller

--- app/Http/Controllers/authentications/LoginController.php
+++ app/Http/Controllers/authentications/LoginController.php
@@ -36,6 +36,6 @@ class LoginController extends Controller
 
     $request->session()->regenerate();
 
-    return redirect()->route('home.index');
+    return redirect()->intended('home.index');
   }
 }
